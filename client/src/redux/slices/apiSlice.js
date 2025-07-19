0a9bcd587455b194fb4501f378fb4ac08b96345a
Luong Tuan Hoang <hoanga2k16lp@gmail.com>
2025-07-19 22:17:14 +0700

    update_api_slice_fe

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -4,7 +4,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const API_URL = import.meta.env.VITE_APP_BASE_URLs;
 // const API_URL = "http://localhost:8800/api";
 
-const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: API_URL,
+  credentials: "include"
+});
 
 export const apiSlice = createApi({
   baseQuery,
